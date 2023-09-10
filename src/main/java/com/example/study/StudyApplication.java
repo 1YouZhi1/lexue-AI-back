c88ba44ecd3998a351119509fe6b79efe0b75e7d
1YouZhi1 <980507010@qq.com>
2023-09-10 16:17:23 +0800

    添加如下注解 @MapperScan("com.example.study.dao.mapper") @EnableCaching @EnableScheduling @Slf4j

--- src/main/java/com/example/study/StudyApplication.java
+++ src/main/java/com/example/study/StudyApplication.java
@@ -1,8 +1,19 @@
 package com.example.study;
 
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author YouZhi
+ */
+@MapperScan("com.example.study.dao.mapper")
+@EnableCaching
+@EnableScheduling
+@Slf4j
 @SpringBootApplication
 public class StudyApplication {
 
