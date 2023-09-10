ee74568c115134fe9168bb8055a80409040337d8
1YouZhi1 <980507010@qq.com>
2023-09-10 19:46:44 +0800

    新增 用户注册 响应类 UserRegisterRespDto

---
+++ src/main/java/com/example/study/dto/resp/UserRegisterRespDto.java
@@ -0,0 +1,21 @@
+package com.example.study.dto.resp;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * 用户注册 响应类
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 10 - 19:33
+ */
+@Data
+@Builder
+public class UserRegisterRespDto {
+
+    private String id;
+
+    private String token;
+
+
+}
