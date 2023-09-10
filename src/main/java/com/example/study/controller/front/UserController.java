66dc40d6652955d3664101ef6b7d5adc7ae907c7
1YouZhi1 <980507010@qq.com>
2023-09-10 16:18:04 +0800

    新增UserController

---
+++ src/main/java/com/example/study/controller/front/UserController.java
@@ -0,0 +1,18 @@
+package com.example.study.controller.front;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 前台门户-用户模块-API控制类
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 10 - 16:03
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/front/user")
+public class UserController {
+
+}
