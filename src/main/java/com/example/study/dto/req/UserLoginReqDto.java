38be043ae16228bea3fef787a6d1c1c9568a169e
1YouZhi1 <980507010@qq.com>
2023-09-10 18:04:44 +0800

    新增 UserLoginReqDto 类

---
+++ src/main/java/com/example/study/dto/req/UserLoginReqDto.java
@@ -0,0 +1,20 @@
+package com.example.study.dto.req;
+
+/**
+ * 用户登录 接收DTO
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 10 - 18:03
+ */
+public class UserLoginReqDto {
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+}
