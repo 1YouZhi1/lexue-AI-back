948df0715302bc4f438f7be7e96c325ca1b9f0f6
1YouZhi1 <980507010@qq.com>
2023-09-10 16:18:15 +0800

    新增UserService 与实现类

---
+++ src/main/java/com/example/study/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.study.service;
+
+/**
+ * 用户模块 服务类
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 10 - 16:01
+ */
+public interface UserService {
+}
---
+++ src/main/java/com/example/study/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.study.service.impl;
+
+import com.example.study.service.UserService;
+
+/**
+ * 用户模块 服务实现类
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 10 - 16:02
+ */
+public class UserServiceImpl implements UserService {
+}
