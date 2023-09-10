0f73a2ba93da0ef9bce97a814b56651e1e54a0e4
1YouZhi1 <980507010@qq.com>
2023-09-10 16:27:57 +0800

    新增系统配置相关常量 +常量类实例化

---
+++ src/main/java/com/example/study/core/constant/SystemConfigConsts.java
@@ -0,0 +1,20 @@
+package com.example.study.core.constant;
+
+/**
+ * 系统配置相关常量
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 10 - 16:24
+ */
+public class SystemConfigConsts {
+
+    private SystemConfigConsts() {
+        throw new IllegalStateException(CONST_INSTANCE_EXCEPTION_MSG);
+    }
+
+    /**
+     * 常量类实例化
+     */
+    public static final String CONST_INSTANCE_EXCEPTION_MSG = "Constant class";
+
+}
