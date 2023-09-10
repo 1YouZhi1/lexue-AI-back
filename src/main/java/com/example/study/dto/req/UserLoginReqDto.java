9fa72b0b91a982d9cde8d8c986853276c915d299
1YouZhi1 <980507010@qq.com>
2023-09-10 18:30:49 +0800

    增加 @Getter注解 UserLoginReqDto

--- src/main/java/com/example/study/dto/req/UserLoginReqDto.java
+++ src/main/java/com/example/study/dto/req/UserLoginReqDto.java
@@ -1,11 +1,14 @@
 package com.example.study.dto.req;
 
+import lombok.Getter;
+
 /**
  * 用户登录 接收DTO
  *
  * @Author YouZhi
  * @Date 2023 - 09 - 10 - 18:03
  */
+@Getter
 public class UserLoginReqDto {
 
     /**
