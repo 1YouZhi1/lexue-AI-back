aaf1bcdb16f7956e11065725f0e64a65fe834687
1YouZhi1 <980507010@qq.com>
2023-09-11 16:14:03 +0800

    生成sys_user 表的对应dao等

---
+++ src/main/java/com/example/study/dao/mapper/SysUserMapper.java
@@ -0,0 +1,16 @@
+package com.example.study.dao.mapper;
+
+import com.example.study.dao.entity.SysUser;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 系统用户 Mapper 接口
+ * </p>
+ *
+ * @author YouZhi
+ * @date 2023/09/11
+ */
+public interface SysUserMapper extends BaseMapper<SysUser> {
+
+        }
