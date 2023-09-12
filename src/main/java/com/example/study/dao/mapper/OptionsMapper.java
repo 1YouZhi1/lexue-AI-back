5711486a389c71e3dacf57c3747e7a7a17863776
1YouZhi1 <980507010@qq.com>
2023-09-12 16:30:55 +0800

    生成选择表dao

---
+++ src/main/java/com/example/study/dao/mapper/OptionsMapper.java
@@ -0,0 +1,16 @@
+package com.example.study.dao.mapper;
+
+import com.example.study.dao.entity.Options;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author YouZhi
+ * @date 2023/09/12
+ */
+public interface OptionsMapper extends BaseMapper<Options> {
+
+        }
