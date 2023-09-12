3644a6077c6f3d80c9a74ee62e96e9cebc6b978e
1YouZhi1 <980507010@qq.com>
2023-09-12 12:57:31 +0800

    生成news_info 表的对应等

---
+++ src/main/java/com/example/study/dao/mapper/NewsInfoMapper.java
@@ -0,0 +1,16 @@
+package com.example.study.dao.mapper;
+
+import com.example.study.dao.entity.NewsInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 新闻信息 Mapper 接口
+ * </p>
+ *
+ * @author YouZhi
+ * @date 2023/09/12
+ */
+public interface NewsInfoMapper extends BaseMapper<NewsInfo> {
+
+        }
