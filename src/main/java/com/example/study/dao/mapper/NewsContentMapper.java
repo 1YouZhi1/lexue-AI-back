5d19b2a0bfa9eb9695769a8c27ba30a808b14be0
1YouZhi1 <980507010@qq.com>
2023-09-12 12:57:13 +0800

    生成news_content 表的对应等

---
+++ src/main/java/com/example/study/dao/mapper/NewsContentMapper.java
@@ -0,0 +1,16 @@
+package com.example.study.dao.mapper;
+
+import com.example.study.dao.entity.NewsContent;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 新闻内容 Mapper 接口
+ * </p>
+ *
+ * @author YouZhi
+ * @date 2023/09/12
+ */
+public interface NewsContentMapper extends BaseMapper<NewsContent> {
+
+        }
