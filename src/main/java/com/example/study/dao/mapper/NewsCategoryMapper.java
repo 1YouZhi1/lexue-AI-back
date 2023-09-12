b10dc1e78899c98cca5910c0210457a991e875b6
1YouZhi1 <980507010@qq.com>
2023-09-12 12:44:06 +0800

    新增新闻类别dao

---
+++ src/main/java/com/example/study/dao/mapper/NewsCategoryMapper.java
@@ -0,0 +1,16 @@
+package com.example.study.dao.mapper;
+
+import com.example.study.dao.entity.NewsCategory;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 新闻类别 Mapper 接口
+ * </p>
+ *
+ * @author YouZhi
+ * @date 2023/09/12
+ */
+public interface NewsCategoryMapper extends BaseMapper<NewsCategory> {
+
+        }
