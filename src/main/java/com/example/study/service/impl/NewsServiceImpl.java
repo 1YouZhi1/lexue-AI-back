615614b55fad89cd41f6b177ac758d61990f5055
1YouZhi1 <980507010@qq.com>
2023-09-12 12:58:05 +0800

    +NewsServiceImpl 新闻模块 响应类

---
+++ src/main/java/com/example/study/service/impl/NewsServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.study.service.impl;
+
+import com.example.study.service.NewsService;
+
+/**
+ * 新闻模块 响应类
+ *
+ * @Author YouZhi
+ * @Date 2023 - 09 - 12 - 12:53
+ */
+public class NewsServiceImpl implements NewsService {
+
+}
