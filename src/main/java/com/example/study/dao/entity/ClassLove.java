18f6dc0916fbe5b5458efe4b09626249f7beda03
1YouZhi1 <980507010@qq.com>
2023-09-24 15:46:52 +0800

    生成了class_love 课程收藏表

---
+++ src/main/java/com/example/study/dao/entity/ClassLove.java
@@ -0,0 +1,73 @@
+package com.example.study.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Builder;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author YouZhi
+ * @date 2023/09/24
+ */
+@TableName("class_love")
+@Builder
+public class ClassLove implements Serializable {
+
+private static final long serialVersionUID = 1L;
+
+        /**
+         * id自增
+         */
+                @TableId(value = "id", type = IdType.AUTO)
+                private Long id;
+
+        /**
+         * 用户id
+         */
+        private Long uId;
+
+        /**
+         * 课程id
+         */
+        private Long cId;
+
+
+    public Long getId() {
+            return id;
+            }
+
+        public void setId(Long id) {
+            this.id = id;
+            }
+
+    public Long getuId() {
+            return uId;
+            }
+
+        public void setuId(Long uId) {
+            this.uId = uId;
+            }
+
+    public Long getcId() {
+            return cId;
+            }
+
+        public void setcId(Long cId) {
+            this.cId = cId;
+            }
+
+@Override
+public String toString() {
+        return "ClassLove{" +
+                "id=" + id +
+                ", uId=" + uId +
+                ", cId=" + cId +
+        "}";
+        }
+        }
---
+++ src/main/java/com/example/study/dao/mapper/ClassLoveMapper.java
@@ -0,0 +1,16 @@
+package com.example.study.dao.mapper;
+
+import com.example.study.dao.entity.ClassLove;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author YouZhi
+ * @date 2023/09/24
+ */
+public interface ClassLoveMapper extends BaseMapper<ClassLove> {
+
+        }
